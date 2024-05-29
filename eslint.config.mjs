92dd1efd57b00c8601fbf73e878eb89fbf8d34d3
RyadC <r.chair@hotmail.fr>
2024-05-29 09:08:48 +0200

    :memo: :package: build(root): Init project

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import js from "@eslint/js";
+
+
+export default [
+  {
+    languageOptions: { 
+      globals: {
+        ...globals.browser, 
+        ...globals.node
+      } 
+    }
+  },
+  js.configs.recommended,
+  pluginJs.configs.recommended,
+];
